758bf74ed4f41cb29481e721fd41726a24d06d42
David Alexander <dga.1984@gmail.com>
2018-03-26 19:36:31 +0100

    Update Fishnet.R
    Better way to create the fishnet

--- Fishnet.R
+++ Fishnet.R
@@ -4,40 +4,39 @@
 setwd("~/Fishnet")
 
 ## Load the Packages
-library(rgdal)
 library(raster)
-#library(rgeos)
-#library(dismo)
-#library(sp)
 library(proj4)
-library(maptools)
-library(GIStools)
+library(GISTools)
 
 ## Setup Boundary and Fishnet ##
 
 # Set layer CRS
-bng = '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs'
+bng <- '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs'
 
 # Load a .shp file
 pdnp <- shapefile("PDNP.shp")
 
 # Change to British National Grid
-proj4string(pdnp)<-(bng)
+pdnp <- spTransform(pdnp, bng)
 
-# Take a look
-plot(pdnp)
+# Create a bounding box
+bb <- bbox(pdnp)
 
-# Create an empty Raster
-grid <- raster(extent(pdnp))
+# Set the dimensions
+grd <- GridTopology(cellcentre.offset = c(bb[1,1], bb[2,1]), cellsize = c(10000,10000), cells.dim = c(5,7))  
 
-# Choose its resolution. 5000 degrees of latitude and longitude.
-res(grid) <- 5000
+# Create the grid layer
+grd.layer <- SpatialPolygonsDataFrame(as.SpatialPolygons.GridTopology(grd), data = data.frame(c(1:35)), match.ID = FALSE)
+names(grd.layer) <- "ID"
 
-# Make the grid have the same coordinate reference system (CRS) as the shapefile.
-proj4string(grid)<-proj4string(pdnp)
+# Lets take a look at the grid
+plot(grd.layer, lty = 2)
 
-# Change to poly
-gridpolygon <- rasterToPolygons(grid)
+# Add some labels
+Lat <- as.vector(coordinates(grd.layer)[,2])
+Lon <- as.vector(coordinates(grd.layer)[,1])
+Names <- as.character(data.frame(grd.layer)[,1])
+pl <- pointLabel(Lon, Lat, Names, offset = 0, cex = .7)
 
-# Overlay the Grid
-plot(gridpolygon, add = T)
+# Overlay the Polygon
+plot(pdnp, add = T)
